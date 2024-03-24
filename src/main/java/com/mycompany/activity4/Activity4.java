24841a04631165cdd5c94d2b0cbfd4423a6224e1
pc <pc@DESKTOP-FV3666P.mshome.net>
2024-03-24 13:34:40 +0800

    you

---
+++ src/main/java/com/mycompany/activity4/Activity4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.activity4;
+
+/**
+ *
+ * @author pc
+ */
+public class Activity4 {
+
+    public static void main(String[] args) {
+        
+        int entranceScore = 75;
+        double gpa = 3.5;
+        
+        if (entranceScore == gpa) {
+            System.out.println("Eligible for admission.");
+        }
+        else {
+            System.out.println("Not eligible for admission.");
+        }
+    }
+}
